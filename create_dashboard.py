d551e0e722be79c84b50d4f4a4717552944c25c1
Surendra Shiwakoti <63626177+shiwakotisurendra@users.noreply.github.com>
2023-07-07 03:56:25 +0200

    modified imports

--- create_dashboard.py
+++ create_dashboard.py
@@ -1,5 +1,4 @@
 import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import matplotlib.pyplot as plt
 from PIL import Image
